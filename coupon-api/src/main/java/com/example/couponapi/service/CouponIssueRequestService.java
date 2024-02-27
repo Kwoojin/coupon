0fe316e533d36979afb5471524729eec866bbc2d
woojinkim <dnwls488@naver.com>
2024-02-28 00:44:58 +0900

    레디스 분산락 및 MYSQL X락 테스트

--- coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.example.couponapi.service;
 
 import com.example.couponapi.controller.dto.CouponIssueRequestDto;
+import com.example.couponcore.components.DistributeLockExecutor;
 import com.example.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +13,13 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
+//        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () ->
+//                couponIssueService.issue(requestDto.couponId(), requestDto.userId())
+//        );
         couponIssueService.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료. couponId: {}, userId: {}", requestDto.couponId(), requestDto.userId());
 
